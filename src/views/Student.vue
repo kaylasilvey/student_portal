85e57571bb089280ef5338abf096db54ed7a89e4
Travis Knight <travis.a.knight@gmail.com>
2019-08-19 20:01:09 -0500

    Add fake data.

--- src/views/Student.vue
+++ src/views/Student.vue
@@ -23,7 +23,39 @@ export default {
         blog: "...@blog.com",
         resumeURL: "...@resume.com",
         gitHub: "...@gitHub.com",
-        photo: "..."
+        photo: "...",
+        experience: [
+          {
+            startDate: "1/1/1901",
+            endDate: "1/2/1902",
+            jobTitle: "Headmaster",
+            company: "Hogwarts",
+            details: "... .... ..."
+          },
+          {
+            startDate: "1/2/1902",
+            endDate: "8/19/2019",
+            jobTitle: "Headmaster",
+            company: "Actualize",
+            details: "... .... ..."
+          }
+        ],
+        education: [
+          {
+            startDate: "9/1/1894",
+            endDate: "1/1/1901",
+            degree: "Muggle Studies",
+            university: "Hogwarts",
+            details: "... .... ..."
+          }
+        ],
+        skills: ["Ruby", "HTML", "Potions", "Strong Earth Magnets", "Arithmancy"],
+        capstone: {
+          name: "Magical Applications of Computer Programming",
+          description: "...",
+          url: "capstone@hogwarts.edu",
+          screenshot: "..."
+        }
       }
     };
   },
