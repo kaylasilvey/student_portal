c51645b068ffddcb7ac472346a9c92e1ef1a8b12
Travis Knight <travis.a.knight@gmail.com>
2019-08-19 19:46:54 -0500

    Adds student data.

--- src/views/Student.vue
+++ src/views/Student.vue
@@ -7,13 +7,31 @@
 <style></style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      student: {
+        firstName: "Peter",
+        lastName: "Jang",
+        email: "peter@email.com",
+        phoneNumber: "123-456-7890",
+        bio: "...",
+        linkedinURL: "...@linkedin.com",
+        twitterHandle: "@PJCoolIce",
+        blog: "...@blog.com",
+        resumeURL: "...@resume.com",
+        gitHub: "...@gitHub.com",
+        photo: "..."
+      }
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("https://jsonplaceholder.typicode.com/posts/1").then(response => {
+      this.student = response.data;
+    });
+  },
   methods: {}
 };
 </script>
